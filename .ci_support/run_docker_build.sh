42f549d7d65bb9c349d417ff5e77d3888e686bdc
Julien Schueller <schueller@phimeca.com>
2020-04-19 09:19:04 +0200

    Move ot build to Dockerfile

--- .ci_support/run_docker_build.sh
+++ .ci_support/run_docker_build.sh
@@ -2,13 +2,9 @@
 
 set -xe
 
-git clone https://github.com/openturns/openturns.git /tmp/openturns
-cd /tmp/openturns
-cmake -DCMAKE_INSTALL_PREFIX=~/.local -DBUILD_PYTHON=OFF -DUSE_COTIRE=ON -DCOTIRE_MAXIMUM_NUMBER_OF_UNITY_INCLUDES="-j16" .
-make install
-
 cd /tmp
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/.local -DOpenTURNS_DIR=~/.local/lib/cmake/openturns /io
+cmake -DCMAKE_INSTALL_PREFIX=~/.local /io
 make install
 ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
+
